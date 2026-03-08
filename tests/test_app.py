f3048883ae32181fe07578f438af1f8934e86fbe
Pragya-Malik <malikpragya02@gmail.com>
2026-03-08 15:05:43 +0530

    modify the flake8 rules

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from app import app
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 def test_home():
